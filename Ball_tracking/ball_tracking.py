ad2fd587d643013ba761ae3db26a75e8ddf45312
Tarun Sai Reddy <105339107+tarunreddyy@users.noreply.github.com>
2023-02-20 12:23:41 -0500

    Add files via upload

--- Ball_tracking/ball_tracking.py
+++ Ball_tracking/ball_tracking.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import image
+
+x_center = []
+y_center = []
 
 
 def ret_images(file):
@@ -13,8 +17,10 @@ def ret_images(file):
         ret, frame = cap.read()
         if not ret:
             break
+
         # Convert frame to HSV color space
         images.append(frame)
+
     # Release the video
     cap.release()
 
@@ -30,7 +36,7 @@ def find_ball(images):
     for i in range(len(images)):
         hsv = cv2.cvtColor(images[i], cv2.COLOR_BGR2HSV)
 
-        l_1 = np.array([0, 200, 50])
+        l_1 = np.array([0, 200, 100])
         u_1 = np.array([10, 255, 255])
 
         l_2 = np.array([160, 100, 20])
@@ -39,25 +45,17 @@ def find_ball(images):
         lower_mask = cv2.inRange(hsv, l_1, u_1)
         upper_mask = cv2.inRange(hsv, l_2, u_2)
 
+        # Mask generated for frame
         full_mask = lower_mask + upper_mask
-        contours, _ = cv2.findContours(
-            full_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        # Extracting x and y coordinates from an image
-        # Find the largest contour (assume this is the ball)
-        max_area = 0
-        largest_contour = None
-        for cnt in contours:
-            area = cv2.contourArea(cnt)
-            if area > max_area:
-                max_area = area
-                largest_contour = cnt
 
-        # Check if largest contour is not None
-        if largest_contour is not None:
-            # Calculate center of contour
-            M = cv2.moments(largest_contour)
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
+        # Find the pixels that belong to the ball
+        ball_pixels = np.nonzero(full_mask)
+
+        # Check if there are any pixels that belong to the ball
+        if ball_pixels[0].size != 0 and ball_pixels[1].size != 0:
+            # Calculate the center of the ball
+            cx = np.mean(ball_pixels[1])
+            cy = np.mean(ball_pixels[0])
 
             center_points_x.append(cx)
             center_points_y.append(cy)
@@ -79,9 +77,9 @@ def fit_parabolic_curve(center_points_x, center_points_y):
     coeff = np.linalg.solve(ATA, ATy)
     a, b, c = coeff
 
-    x = np.linspace(min(center_points_x), max(center_points_x), 10)
+    x = np.linspace(min(center_points_x), max(center_points_x), 100)
     y = a*x**2 + (b*x) + c
-
+    print(f"y = {a}*x**2 + {b}*x + {c}")
     return a, b, c, x, y
 
 
@@ -92,18 +90,28 @@ def main():
     x_center, y_center = find_ball(All_images)
 
     a, b, c, x, y = fit_parabolic_curve(x_center, y_center)
-    print(a, b, c)
+    # print(a,b,c)
 
     landing_y = 300 + y_center[0]
     landing_x = (-b + np.sqrt(b**2 - 4*a*(c - landing_y)))/(2*a)
     print(f"Landing x-coordinate: {landing_x} pixels")
 
-    plt.scatter(x_center, y_center)
+    data = cv2.cvtColor(All_images[55], cv2.COLOR_BGR2RGB)
+    plt.figure(figsize=(15, 10))
+    plt.plot(x, y, color="white", linewidth=2)
+    plt.scatter(x_center, y_center, s=5)
     plt.plot(x, y, '-r')
+    plt.annotate('Ball Trajectory', xy=(x_center[110]+10, y_center[110]),
+                 fontsize=14, xytext=(900, 280),
+                 arrowprops=dict(facecolor='blue'),
+                 color='white')
+    plt.annotate('LSF curve', xy=(x[30], y[30]-5),
+                 fontsize=14, xytext=(200, 200),
+                 arrowprops=dict(facecolor='red'),
+                 color='white')
     plt.axis([min(x_center), max(x_center), max(y_center), 0])
-    plt.xlabel("X Coordinate (pixels)")
-    plt.ylabel("Y Coordinate (pixels)")
-    plt.title("Center Points of Ball")
+    plt.axis('off')
+    plt.imshow(data)
     plt.show()
 
 
